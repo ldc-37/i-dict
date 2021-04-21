fb94282f0c9f5b900274cfa747de744fec33ec38
ldc <lidingcheng@tal.com>
2021-04-22 00:33:22 +0800

    feat(cloud): login云函数逻辑完善

--- cloud/functions/login/index.js
+++ cloud/functions/login/index.js
@@ -1,32 +1,67 @@
-// 云函数模板
-// 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
-
 const cloud = require('wx-server-sdk')
-
 // 初始化 cloud
 cloud.init({
   // API 调用都保持和云函数当前所在环境一致
   env: cloud.DYNAMIC_CURRENT_ENV
 })
+const db = cloud.database()
 
-/**
- * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
- * 
- * event 参数包含小程序端调用传入的 data
- * 
- */
+
 exports.main = async (event, context) => {
-  console.log(event)
-  console.log(context)
+  console.log('event', event)
 
-  // 可执行其他自定义逻辑
-  // console.log 的内容可以在云开发云函数调用日志查看
-
-  // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）等信息
   const wxContext = cloud.getWXContext()
+
+  function getNewUserData(openid) {
+    return {
+      "createAt": new Date(),
+      "finishDate": [],
+      "isVip": false,
+      "lastLoginAt": new Date(),
+      "mark": [],
+      "openid": openid,
+      "progress": {},
+      "setting": {
+        "albumId": 1,
+        "amountPerDay": 10,
+        "dictId": 1,
+        "durationKeepAfterRecite": 1500,
+        "timesToChangeBackground": 1,
+        "tipsDuration": 1000
+      },
+      "syncTime": {
+        "mark": new Date(),
+        "progress": new Date(),
+        "setting": new Date()
+      },
+      "unionid": ""
+    }
+  }
+
+  let userId = ''
+  const userData = await db.collection('user')
+    .where({
+      openid: wxContext.OPENID
+    })
+    .get()
+  if (!userData.length) {
+    const newUserData = getNewUserData(wxContext.OPENID)
+    const newData = await db.collection('user')
+      .add({
+        data: newUserData
+      })
+    userId = newData._id
+    } else {
+    userId = userData._id
+    await db.collection('user')
+      .update({
+        lastLoginAt: new Date()
+      })
+  }
 
   return {
     event,
+    userId,
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
     unionid: wxContext.UNIONID,
